cfd3e08c79de4385ae3e9e6f5f8d406f0ed8ec5a
meimeuiii <152019926+meimeuiii@users.noreply.github.com>
2023-12-02 19:27:49 +0200

    meimi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import*
+from PyQt5.QtWidgets import*
 
 app = QApplication([])
 window = QWidget()
@@ -8,8 +8,17 @@ ans2 = QRadioButton("14")
 
 main_line = QVBoxLayout()
 main_line.addWidget(lbl)
-h1 = QHBoxLayout()
+h2 = QHBoxLayout()
+h2.addWidget(ans1)
+h2.addWidget(ans2)
+main_line.addLayout(h2)
+def true_var_1():
+    msg = QMessageBox()
+    msg.setText("Правильно!")
+    msg.exes()
 
 
+ans1.clicked.connect(true_var_1)
+window.setLayout(main_line)
 window.show()
 app.exec()
